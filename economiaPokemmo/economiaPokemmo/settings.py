191fa35dd13d0f6afb0604e22e098e402d08fe03
Guillermo Cordova <guiller_ch@hotmail.com>
2022-10-23 14:58:35 -0400

    modificacion de rutas static y agregado codigo google

--- economiaPokemmo/economiaPokemmo/settings.py
+++ economiaPokemmo/economiaPokemmo/settings.py
@@ -134,9 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+if DEBUG:
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+       ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #media
 MEDIA_URL='/media/'
