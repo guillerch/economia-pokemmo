389ab298e7df893c227b9a0059bc1b7c420388bc
Guillermo Cordova <guiller_ch@hotmail.com>
2023-02-24 16:21:25 -0400

    se agregaron rutas para creacion de API

--- economiaPokemmo/apps/guias/urls.py
+++ economiaPokemmo/apps/guias/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import GuiaList, GuiaDetail
 
 urlpatterns = [
     path('guias/', views.guias,name='guias'),
+    path('guias-list/', GuiaList.as_view(), name='guia-list'),
+    path('guias-list/<int:pk>/', GuiaDetail.as_view(), name='guia-detail'),
     ]
--- economiaPokemmo/apps/guias/views.py
+++ economiaPokemmo/apps/guias/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Guia
+from rest_framework import generics
+from .serializers import GuiaSerializer
 # Create your views here.
 def guias(request):
     guides = Guia.objects.all()
@@ -7,3 +9,11 @@ def guias(request):
         'guides':guides,
     }
     return render(request,'guias.html',context)
+
+class GuiaList(generics.ListCreateAPIView):
+    queryset = Guia.objects.all()
+    serializer_class = GuiaSerializer
+
+class GuiaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Guia.objects.all()
+    serializer_class = GuiaSerializer
