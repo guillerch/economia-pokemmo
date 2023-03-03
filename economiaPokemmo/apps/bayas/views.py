b37cfb8bdf5c5699d851b458bce17ceb3339b253
Guillermo Cordova <guiller_ch@hotmail.com>
2023-03-03 12:09:47 -0400

    proteccion de rutas agregada

--- economiaPokemmo/apps/bayas/views.py
+++ economiaPokemmo/apps/bayas/views.py
@@ -1,8 +1,10 @@
 from .models import Baya
 from rest_framework import viewsets
 from .serializers import BayaSerializer
-# Create your views here.
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-class BayaViewSet(viewsets.ModelViewSet):
+
+class BayaViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Baya.objects.all()
     serializer_class = BayaSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
--- economiaPokemmo/apps/blog/views.py
+++ economiaPokemmo/apps/blog/views.py
@@ -1,20 +1,27 @@
-from rest_framework import viewsets
-from .models import Category,Post, Point, Paragraph
-from .serializers import CategorySerializer,PostSerializer, PointSerializer, ParagraphSerializer
+from rest_framework import viewsets, permissions
+from .models import Category, Post, Point, Paragraph
+from .serializers import CategorySerializer, PostSerializer, PointSerializer, ParagraphSerializer
 
 
-class CategoryList(viewsets.ModelViewSet):
+class CategoryList(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class PostViewSet(viewsets.ModelViewSet):
+
+class PostViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class PointViewSet(viewsets.ModelViewSet):
+
+class PointViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Point.objects.all()
     serializer_class = PointSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class ParagraphViewSet(viewsets.ModelViewSet):
+
+class ParagraphViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Paragraph.objects.all()
     serializer_class = ParagraphSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
